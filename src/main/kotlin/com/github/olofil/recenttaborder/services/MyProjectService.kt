0c359ad5e5ebba5b33111ec1a3db5e47a3cacbe9
GitHub Action <action@github.com>
2023-02-13 10:34:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/olofil/recenttaborder/services/MyProjectService.kt
+++ src/main/kotlin/com/github/olofil/recenttaborder/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.olofil.recenttaborder.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.olofil.recenttaborder.MyBundle
 
 class MyProjectService(project: Project) {
 
