190cec92475da1e33a177214f353d999fa82fc13
Olof Hedman <olle@illusionlabs.se>
2023-02-28 13:23:14 +0100

    A bit of cleanup

--- src/main/kotlin/com/example/recenttaborder/MyProjectService.kt
+++ src/main/kotlin/com/example/recenttaborder/MyProjectService.kt
@@ -8,10 +8,14 @@ import com.intellij.openapi.fileEditor.FileEditorManagerEvent
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
 import com.intellij.openapi.fileEditor.impl.EditorWindow
+import com.intellij.openapi.fileEditor.impl.EditorWindowHolder
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.WindowManager
 import com.intellij.openapi.wm.ex.ToolWindowManagerListener
 import com.intellij.openapi.wm.impl.ToolWindowManagerImpl
+import com.intellij.psi.impl.source.tree.injected.EditorWindowTracker
+import com.intellij.psi.impl.source.tree.injected.EditorWindowTrackerImpl
 import com.intellij.serviceContainer.ComponentManagerImpl
 import com.intellij.ui.content.ContentManager
 import com.intellij.ui.content.impl.TabbedContentImpl
@@ -23,129 +27,79 @@ import com.intellij.util.messages.MessageBusConnection
 
 class MyProjectService(project: Project) {
 
-    private var messageBusConnection: MessageBusConnection? = null
-
     private var didIt = false
+
+    private var registeredWindows = setOf<EditorWindow>()
+
+    fun registerTabListener(project: Project) {
+        val fex = FileEditorManagerEx.getInstance(project) as FileEditorManagerEx
+
+
+        for (v in fex.windows) {
+            v.tabbedPane.tabs.addListener(object : TabsListener {
+                override fun selectionChanged(oldSelection: TabInfo?, newSelection: TabInfo?) {
+                    super.selectionChanged(oldSelection, newSelection)
+//                    println("Tab selection changed! from $oldSelection to $newSelection")
+                    if(oldSelection != null) {
+                        val newTabs = mutableListOf<TabInfo?>();
+                        newTabs.add(newSelection);
+                        for(tab in v.tabbedPane.tabs.tabs) {
+                            if(tab != newSelection) {
+                                newTabs.add(tab);
+                            }
+                        }
+                        v.tabbedPane.tabs.removeAllTabs()
+                        for(tab in newTabs) {
+                            v.tabbedPane.tabs.addTab(tab);
+                        }
+                    }
+                }
+            })
+        }
+    }
 
     init {
         println("MyProjectService Startup " + project.name)
 
-        messageBusConnection = project.messageBus.connect()
-
-
-/*
-        messageBusConnection?.subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER,
-            object : FileEditorManagerListener {
-
-                override fun selectionChanged(event: FileEditorManagerEvent) {
-
-
-
-                    super.selectionChanged(event)
-                    val openFiles = event.manager.openFiles
-                    if(openFiles.size >= 2) {
-                        // Reorder the open files
-                        val lastFile = openFiles.last()
-                        event.manager.openFile(openFiles.first(), true)
-                        event.manager.closeFile(lastFile)
-
-
-                    }
-                }
-            })
-*/
-/*        val fem = FileEditorManager.getInstance(project)
-        fem.addFileEditorManagerListener(object : FileEditorManagerListener {
-            override fun selectionChanged(event: FileEditorManagerEvent) {
-                super.selectionChanged(event)
-
-                println("Callback: Selection changed in fileEditorManager! from ${event.oldEditor?.file}(${event.oldEditor?.name}) to ${event.newEditor?.file}(${event.newEditor?.name})")
-
-
-            }
-        })
-*/
-
-
-
-//        project.
         EditorFactory.getInstance().addEditorFactoryListener(object : EditorFactoryListener {
 
             override fun editorCreated(event: EditorFactoryEvent) {
                 // Get the tool window manager for the project
-                val toolWindowManager = ToolWindowManager.getInstance(project)
                 // Called when a new editor window is created
-                println("New editor window created with toolwindowmanager $toolWindowManager")
+                println("New editor created ${event.editor}")
 
+                // Haven't found a way to listen for when the EditorWindow is created, so instead I listen for when FileEditor is created.
+                // When the first FileEditor is created I can call FileEditorManagerEx.getInstance(project) and get a list of windows,
+                // I do this only once assuming the list of windows does not change.
                 if(!didIt) {
 
-                    val fex = FileEditorManagerEx.getInstance(project) as FileEditorManagerEx
+                    registerTabListener(project)
 
-                    for (v in fex.windows) {
-                        v.tabbedPane.tabs.addListener(object : TabsListener {
-                            override fun selectionChanged(oldSelection: TabInfo?, newSelection: TabInfo?) {
-                                super.selectionChanged(oldSelection, newSelection)
-
-                                println("Tab selection changed! from $oldSelection to $newSelection")
-
-                                if(oldSelection != null) {
-//                                    v.tabbedPane.tabs.addTab(newSelection, 0);
-//                                    v.tabbedPane.tabs.removeTab(newSelection);
-                                    val newTabs = mutableListOf<TabInfo?>();
-                                    newTabs.add(newSelection);
-                                    for(tab in v.tabbedPane.tabs.tabs) {
-                                        if(tab != newSelection) {
-                                            newTabs.add(tab);
-                                        }
-                                    }
-                                    v.tabbedPane.tabs.removeAllTabs()
-                                    for(tab in newTabs) {
-                                        v.tabbedPane.tabs.addTab(tab);
-                                    }
-                                }
-                            }
-                        })
-                    }
                     didIt = true
                 }
-
-//                event.editor
-
-
-                val ids = toolWindowManager.toolWindowIds;
-                for (id in ids) {
-                    println(id)
-                }
-
-                // Get the tool window containing the tabbed content component
-                val toolWindow = toolWindowManager.getToolWindow("EditorTabs")
-
-                val tabbedContent = toolWindow?.contentManager?.selectedContent // as TabbedContentImpl
-
-
-                // Get all the contents (tabs) in the tabbed content component
-//                val contents = tabbedContent?.tabs;
-
-                println("jeee");
             }
 
             override fun editorReleased(event: EditorFactoryEvent) {
                 // Called when an editor window is closed
+                println("Editor released ${event.editor}");
             }
         }, project);
 
 
+        // Below is a lot of approaches tried that seems they don't work...
+        // Either straight up does not work, or is using deprecated calls.
+
 //        ToolWindowManagerListener mm
 
         // Get the tool window manager for the project
-        val toolWindowManager = ToolWindowManager.getInstance(project) as ToolWindowManagerImpl
+//        val toolWindowManager = ToolWindowManager.getInstance(project) as ToolWindowManagerImpl
 
 //        toolWindowManager.toolWindowsPane
 //
-//        toolWindowManager.addToolWindowManagerListener(ob)
+//        toolWindowManager.addToolWindowManagerListener()
 
         // Get the tool window containing the tabbed content component
-        val toolWindow = toolWindowManager.getToolWindow("EditorTabs")
+//        val toolWindow = toolWindowManager.getToolWindow("EditorTabs")
 
 
         // Get the tabbed content component from the tool window
@@ -163,8 +117,6 @@ class MyProjectService(project: Project) {
 
         // Get all the contents (tabs) in the tabbed content component
   //      val contents = tabbedContent?.tabs;
-
-
 
         /*
 
