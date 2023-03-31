f10148859da847e20d40ab98c777e399dd535aea
Olof Hedman <olle@illusionlabs.se>
2023-03-31 16:39:57 +0200

    Cleanup of stale code and disabled debug prints

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "se.illusionlabs"
-version = "1.0.1"
+version = "1.0.2"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/se/illusionlabs/recenttaborder/MyProjectService.kt
+++ src/main/kotlin/se/illusionlabs/recenttaborder/MyProjectService.kt
@@ -3,33 +3,17 @@ package se.illusionlabs.recenttaborder
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.event.EditorFactoryEvent
 import com.intellij.openapi.editor.event.EditorFactoryListener
-import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.fileEditor.FileEditorManagerEvent
-import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
-import com.intellij.openapi.fileEditor.impl.EditorWindow
-import com.intellij.openapi.fileEditor.impl.EditorWindowHolder
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.ToolWindowManager
-import com.intellij.openapi.wm.WindowManager
-import com.intellij.openapi.wm.ex.ToolWindowManagerListener
-import com.intellij.openapi.wm.impl.ToolWindowManagerImpl
-import com.intellij.psi.impl.source.tree.injected.EditorWindowTracker
-import com.intellij.psi.impl.source.tree.injected.EditorWindowTrackerImpl
-import com.intellij.serviceContainer.ComponentManagerImpl
-import com.intellij.ui.content.ContentManager
-import com.intellij.ui.content.impl.TabbedContentImpl
-import com.intellij.ui.tabs.JBTabs.SelectionChangeHandler
 import com.intellij.ui.tabs.TabInfo
 import com.intellij.ui.tabs.TabsListener
-import com.intellij.util.messages.MessageBusConnection
 
 
 class MyProjectService(project: Project) {
 
     private var didIt = false
 
-    private var registeredWindows = setOf<EditorWindow>()
+    private var printDEBUG = false
 
     private var processing = false;
 
@@ -43,7 +27,7 @@ class MyProjectService(project: Project) {
                     if(!processing) { // Handle unwanted recursion when removing and adding tabs.
                         processing = true
                         super.selectionChanged(oldSelection, newSelection)
-                        println("Tab selection changed! from $oldSelection to $newSelection")
+                        if(printDEBUG) println("Tab selection changed! from $oldSelection to $newSelection")
                         if (oldSelection != null) {
                             val newTabs = mutableListOf<TabInfo?>();
                             newTabs.add(newSelection);
@@ -65,14 +49,14 @@ class MyProjectService(project: Project) {
     }
 
     init {
-        println("MyProjectService Startup " + project.name)
+        if(printDEBUG) println("MyProjectService Startup " + project.name)
 
         EditorFactory.getInstance().addEditorFactoryListener(object : EditorFactoryListener {
 
             override fun editorCreated(event: EditorFactoryEvent) {
                 // Get the tool window manager for the project
                 // Called when a new editor window is created
-                println("New editor created ${event.editor}")
+                if(printDEBUG) println("New editor created ${event.editor}")
 
                 // Haven't found a way to listen for when the EditorWindow is created, so instead I listen for when FileEditor is created.
                 // When the first FileEditor is created I can call FileEditorManagerEx.getInstance(project) and get a list of windows,
@@ -87,7 +71,7 @@ class MyProjectService(project: Project) {
 
             override fun editorReleased(event: EditorFactoryEvent) {
                 // Called when an editor window is closed
-                println("Editor released ${event.editor}");
+                if(printDEBUG) println("Editor released ${event.editor}");
             }
         }, project);
     }
